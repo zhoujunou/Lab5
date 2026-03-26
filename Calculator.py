8f8abfcbe930dd34abc6147bb9a3c3a5671fe96a
zhoujunou <3489567467@qq.com>
2026-03-26 14:05:09 +0800

    Initial commit

---
+++ Calculator.py
@@ -0,0 +1,57 @@
+# This function adds two numbers
+# NAME: M McMahon
+
+def add(x, y):
+    return x + y
+
+# This function subtracts two numbers
+def subtract(x, y):
+    return x - y
+
+# This function multiplies two numbers
+def multiply(x, y):
+    return x * y
+
+# This function divides two numbers
+def divide(x, y):
+    return x / y
+
+
+print("Select operation.")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+
+while True:
+    # take input from the user
+    choice = input("Enter choice(1/2/3/4): ")
+
+    # check if choice is one of the four options
+    if choice in ('1', '2', '3', '4'):
+        try:
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+            continue
+
+        if choice == '1':
+            print(num1, "+", num2, "=", add(num1, num2))
+
+        elif choice == '2':
+            print("You enter code for #2 here")
+            # CODE HERE ENTERED
+        elif choice == '3':
+            print("You enter code for #3 here")
+            
+        elif choice == '4':
+            print("You enter code for #4 here")
+            
+        # check if user wants another calculation
+        # break the while loop if answer is no
+        next_calculation = input("Let's do next calculation? (yes/no): ")
+        if next_calculation == "no":
+          break
+    else:
+        print("Invalid Input")
---
+++ README.md
@@ -0,0 +1,17 @@
+# Python_Calculator - Output
+
+In this program, we ask the user to choose an operation. Options 1, 2, 3, and 4 are valid. If any other input is given, Invalid Input is displayed and the loop continues until a valid option is selected.
+
+Two numbers are taken and an if...elif...else branching is used to execute a particular section. User-defined functions add(), subtract(), multiply() and divide() evaluate respective operations and display the output.
+
+
+Select operation.
+1.Add
+2.Subtract
+3.Multiply
+4.Divide
+Enter choice(1/2/3/4): 3
+Enter first number: 15
+Enter second number: 14
+15.0 * 14.0 = 210.0
+Let's do next calculation? (yes/no): no
